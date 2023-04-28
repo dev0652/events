465f9538ab6b7efb454196e1159a0567004dd304
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-04-29 02:00:53 +0300

    update files

---
+++ src/components/Event/Event.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import css from './Event.module.css';
+
+export const Event = ({ name, location, speaker, type, start, end }) => {
+  <div className="css.event">
+    <h2 className="css.title">{name}</h2>
+    <p className="css.info">
+      <i className="css.icon"></i>
+      {Location}
+    </p>
+
+    <p className="css.info">
+      <i className="css.icon"></i>
+      {speaker}
+    </p>
+
+    <p className="css.info">
+      <i className="css.icon"></i>
+      {start}
+    </p>
+
+    <p className="css.info">
+      <i className="css.icon"></i>
+      Duration
+    </p>
+
+    {/* <span className="chip free|paid|vip">{type}</span> */}
+  </div>;
+};
+
+Event.propTypes = {
+  name: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  speaker: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  start: PropTypes.string.isRequired,
+  end: PropTypes.string.isRequired,
+};
