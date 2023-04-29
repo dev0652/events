9c8541683bb0b033f7959e91c48cb20bb80c6079
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-04-29 23:54:25 +0300

    123

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
-import { FaMapMarkerAlt, FaUserAlt, FaCalendarAlt, FaClock } from 'react-icons/fa';
-import {formatEventStart} from 'utils/formatEventStart';
-import {formatEventDuration} from 'utils/formatDuration';
+import {
+  FaMapMarkerAlt,
+  FaUserAlt,
+  FaCalendarAlt,
+  FaClock,
+} from 'react-icons/fa';
 import css from './Event.module.css';
 
-
+import { formatEventStart, formatEventDuration } from 'utils';
 
 export const Event = ({ name, location, speaker, type, start, end }) => {
   const formattedStart = formatEventStart(start);
@@ -12,31 +15,30 @@ export const Event = ({ name, location, speaker, type, start, end }) => {
 
   return (
     <div className={css.event}>
-    <h2 className={css.title}>{name}</h2>
-    <p className={css.info}>
-      <FaMapMarkerAlt className={css.icon} size={16}/>
-      {location}
-    </p>
+      <h2 className={css.title}>{name}</h2>
+      <p className={css.info}>
+        <FaMapMarkerAlt className={css.icon} size={16} />
+        {location}
+      </p>
 
-    <p className={css.info}>
-      <FaUserAlt className={css.icon} size={16}/>
-      {speaker}
-    </p>
+      <p className={css.info}>
+        <FaUserAlt className={css.icon} size={16} />
+        {speaker}
+      </p>
 
-    <p className={css.info}>
-      <FaCalendarAlt className={css.icon} size={16}/>
-      {formattedStart}
-    </p>
+      <p className={css.info}>
+        <FaCalendarAlt className={css.icon} size={16} />
+        {formattedStart}
+      </p>
 
-    <p className={css.info}>
-      <FaClock className={css.icon} size={16}/>
-      {duration}
-    </p>
+      <p className={css.info}>
+        <FaClock className={css.icon} size={16} />
+        {duration}
+      </p>
 
-    {/* <span className="chip free|paid|vip">{type}</span> */}
-  </div>
-  )
-  
+      {/* <span className="chip free|paid|vip">{type}</span> */}
+    </div>
+  );
 };
 
 Event.propTypes = {
