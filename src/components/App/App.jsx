1283c99dc55850abc926f5893a4a3e53399db7bd
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-05-01 20:47:30 +0300

    132

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import upcomingEvents from '../upcoming-events.json';
+import upcomingEvents from '../../upcoming-events.json';
 import { PageTitle } from 'components/PageTitle/PageTitle';
 import { EventBoard } from 'components/EventBoard/EventBoard';
 
